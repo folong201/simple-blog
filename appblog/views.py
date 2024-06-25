b3f9f82db3bba826cfbed5276c5dd49cd3f41f4a
folong201 <folong201@gmail.com>
2024-06-25 20:14:58 +0100

    ceation of the article

--- appblog/views.py
+++ appblog/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render,redirect
+from . models import Article,CustomUser
 
 # Create your views here.
 def index(request):
-    return render (request,"index.html")
+    articles = Article.objects.all()
+    
+    return render (request,"index.html",{'articles':articles})
 
 def updatearticle(request):
     return render (request,"updatearticle.html")
 
 def createarticle(request):
+    if request.method == 'POST':
+        article = Article()
+        print('authuser')
+        print(request.user)
+        author = CustomUser.objects.get(id = request.user.id)
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.author = author
+        article.save()
+        return redirect("index")
+        
+        
     return render (request,"createarticle.html")
 
 def articledetail(request):
@@ -21,7 +36,8 @@ def userdashboard(request):
     return render (request,"userdashboard.html")
 
 def admindashboard(request):
-    return render (request,"admindashboard.html")
+    articles = Article.objects.all()   
+    return render (request,"admindashboard.html",{'articles':articles})
 
 def login(request):
     return render (request,"auth/login.html")
